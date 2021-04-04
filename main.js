cd2dc7f3b431fa97de74bcb5b6e8f2c261f0d0ab
forya90 <tariq_esa90@yahoo.com>
2021-04-04 22:10:00 +0100

    added genesis and two other blocks

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
-cost SHA356 = require ('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 
-class Block{
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index; 
-        this.timestamp = timestamp; 
-        this.data = data; 
-        this.previousHash = previousHash;
-        this.hash = '';
-    }
-    calculateHash(){
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
 
-    }
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+  createGenesisBlock() {
+    return new Block(0, "04/04/2021", "SharwallLape", " ");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let SharwallCoin = new Blockchain();
+SharwallCoin.addBlock(new Block(1, "04/04/2021", { amount: 4 }));
+SharwallCoin.addBlock(new Block(2, "05/04/2021", { amount: 8 }));
+
+console.log(JSON.stringify(SharwallCoin, null, 4));
