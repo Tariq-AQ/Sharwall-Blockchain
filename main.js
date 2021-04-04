08536c6cc09005bc2c92903b9dd666eabf36b4bb
forya90 <tariq_esa90@yahoo.com>
2021-04-04 21:10:14 +0100

    installed crypto-js and added it to main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+cost SHA356 = require ('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index; 
+        this.timestamp = timestamp; 
+        this.data = data; 
+        this.previousHash = previousHash;
+        this.hash = '';
+    }
+    calculateHash(){
+
+    }
+}
